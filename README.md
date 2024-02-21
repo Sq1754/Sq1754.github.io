07ac47f4883aa6d0d61e3386e5c37fcb1ddeae8a
Sq1754 <Saqlain4517@gmail.com>
2024-02-21 19:05:14 +0530

    resume update And README update

--- README.md
+++ README.md
@@ -1,10 +1,35 @@
-# Sq1754.github.io
 
-Tic Tac Toe :- 
+
+
+
+
+# Portfolio Website
+
+This is a portfolio website template for showcasing your skills, projects, and contact information.
+
+## Projects output links
+```
+<h2>Tic Tac Toe :- </h2>
 https://sq1754.me/TicTacToe/
 
-Snake Game :-
+<h2>Snake Game :-</h2>
 https://sq1754.me/SnakeGame/
 
-Food Restaurant Website:-
+<h2>Food Restaurant Website:-</h2>
 https://sq1754.me/CheffieFood
+
+<h2>Music-Player</h2>
+https://sq1754.me/SpotifyClone
+
+```
+
+## Description
+
+This portfolio website template is designed for web developers to present their skills, projects, and contact information in a professional manner. It includes sections for home, about, services, skills, projects, and contact.
+
+## Features
+
+- Responsive design for various screen sizes
+- Navigation menu with smooth scrolling
+- Sections for showcasing skills, projects, and contact information
+- Integration with social media and external links
--- main.js
+++ main.js
@@ -8,6 +8,45 @@ const toggleMenu = () => {
     const iconName = isBurger ?
     'images/cross.png': 'images/ham.png';
 
+
+/* Animate On Scroll
+    * ------------------------------------------------------ */
+var ssAOS = function() {     
+    AOS.init( {
+        offset: 200,
+        duration: 600,
+        easing: 'ease-in-sine',
+        delay: 300,
+        once: true,
+        disable: 'mobile'
+    });
+};
+
+/* Preloader
+    * -------------------------------------------------- */
+var ssPreloader = function() {
+        
+    $("html").addClass('ss-preload');
+
+    $WIN.on('load', function() {
+
+        //force page scroll position to top at page refresh
+        $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+        // will first fade out the loading animation 
+        $("#loader").fadeOut("slow", function() {
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
+        }); 
+        
+        // for hero content animations 
+        $("html").removeClass('ss-preload');
+        $("html").addClass('ss-loaded');
+    
+    });
+};
+
+
     burgerMenu.setAttribute(
         'src' , iconName
     );
